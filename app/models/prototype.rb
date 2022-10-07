2694d2ce4e76425343f12d21b8d1091612e60073
donrow007 <wn.nw106@gmail.com>
2022-10-07 16:43:35 +0900

    バリデーションを設定しましょう

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,2 +1,8 @@
 class Prototype < ApplicationRecord
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :name, presence: true
+  validates :profile, presence: true
+  validates :occupation, presence: true
+  validates :position, presence: true
 end
